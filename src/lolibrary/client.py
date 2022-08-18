b31a0c6e59791e3d5161ec4869b6a94087bba2db
GriffinSchulte <griffinschulte@gmail.com>
2022-08-18 13:48:51 -0500

    Add client class init and GET wrapper.

---
+++ src/lolibrary/client.py
@@ -0,0 +1,42 @@
+"""League of Legends API Client Library"""
+import json
+
+from loguru import logger
+from .session import LoLibrarySession
+
+
+class LoLibrary:
+    """League of Legends API Client Class Wrapper"""
+    def __init__(self, host, token):
+        self.host = host.rstrip('/')
+        self.token = token
+        self.session = LoLibrarySession(self.host, self.token)
+
+    def _get(self, endpoint, params: dict = None):
+        """Private method for wrapping HTTP GET requests.
+        
+        args:
+            endpoint (str): Endpoint URL.
+            params (dict): Optional. Parameter dictionart to be passed in the URL.
+
+        returns:
+            response.text: For all successful API calls, the requests.text payload
+            will be returned.
+        """
+        url = self.host + endpoint
+
+        response = self.session.get(url, params=params)
+
+        if response.status_code != 200:
+            # Custom exception to be raised here.
+            result = None
+        else:
+            result = response.text
+
+        return result
+
+##########################################################################
+# API Methods
+##########################################################################
+# Category: 
+# Category Description
